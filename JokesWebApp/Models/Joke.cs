5d9d160d7dc26787a18a592f4d2922480aac47ca
Jaqueline <jaquelineribeiro015@gmail.com>
2023-07-20 22:40:42 -0300

    Projeto dotnet terminado

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+namespace JokesWebApp.Models
+{
+	public class Joke
+	{
+		public int Id { get; set; }
+		public string JokeQuestion { get; set; }
+		public string JokeAnswer { get; set;}
+
+		public Joke()
+		{
+
+		}
+	}
+}
+
